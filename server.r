d0b4d47503a261a89f9ccf293b126ea3371ed8dd
Sandipan Samanta <42437305+ssandipansamanta@users.noreply.github.com>
2024-04-11 16:38:35 +0200

    SANDIPAN| Update server.r

--- server.r
+++ server.r
@@ -11,9 +11,7 @@ server <- function(input, output) {
       BAs = input$BAs
     )
   })  
-  
-  
-  
+    
   output$soil_condition <- renderText({
     new_data <- data.frame(
       OC = input$OC,
@@ -25,9 +23,9 @@ server <- function(input, output) {
     RF <- readRDS('model.RData')
     model_prediction <- predict(RF, newdata = new_data, type = "raw")
     if (model_prediction == "<MTC") {
-      soil_condition = HTML('<p> <ol> Rice grain Arsenic Content: <span style="color:green" > <b> LESS </b> </span> than 350 μg/kg as per Codex recommendation </ol> </p>')
+      soil_condition = HTML('<p> The Codex Alimentarius Commission in 2017 has established maximum levels for inorganic arsenic in both husked and polished (white) rice. Rice grain Arsenic Content is <span style="color:green" > <b> LESS </b> </span> than 350 μg/kg in this case. </p>')
     } else if (model_prediction == ">MTC") {
-      soil_condition = HTML('<p> <ol> Rice grain Arsenic Content: <span style="color:red" > <b> MORE </b> </span>than 350 μg/kg as per Codex recommendation </ol> </p>')
+      soil_condition = HTML('<p> The Codex Alimentarius Commission in 2017 has established maximum levels for inorganic arsenic in both husked and polished (white) rice. Rice grain Arsenic Content: <span style="color:red" > <b> MORE </b> </span>than 350 μg/kg as per Codex recommendation. </p>')
     } else {
       soil_condition = 0
     }
